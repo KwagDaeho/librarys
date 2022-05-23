08bc3126ee06c2789011a50cecfbf51c367014c3
Haraho <1438eogh@gmail.com>
2022-05-23 12:29:37 +0900

    add:Styled Components - css selector

--- src/Components/Librarys/StyledComponents.jsx
+++ src/Components/Librarys/StyledComponents.jsx
@@ -1,33 +1,51 @@
 import React from "react";
 import styled from "styled-components";
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+  margin: 20px auto;
+`;
+const Button = styled.button`
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
+const ReversedButton = (props) => (
+  <Button {...props} children={props.children.split("").reverse()} />
+);
+const Thing = styled.div.attrs((/* props */) => ({ tabIndex: 0 }))`
+  color: blue;
+  &:hover {
+    color: red; // <Thing> when hovered
+  }
+  & ~ & {
+    background: tomato; // <Thing> as a sibling of <Thing>, but maybe not directly next to it
+  }
+  & + & {
+    background: lime; // <Thing> next to <Thing> => <Thing> No.2
+  }
+  &.something {
+    background: orange; // <Thing> tagged with an additional CSS class ".something"
+  }
+  .something-else & {
+    border: 1px solid; // <Thing> inside another element labeled ".something-else"
+  }
+`;
 export default function StyledComponents() {
-  const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-  `;
-  const Wrapper = styled.section`
-    padding: 4em;
-    background: papayawhip;
-    margin: 20px auto;
-  `;
-  const Button = styled.button`
-    background: ${(props) => (props.primary ? "palevioletred" : "white")};
-    color: ${(props) => (props.primary ? "white" : "palevioletred")};
-    font-size: 1em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border: 2px solid palevioletred;
-    border-radius: 3px;
-  `;
-  const TomatoButton = styled(Button)`
-    color: tomato;
-    border-color: tomato;
-  `;
-  const ReversedButton = (props) => (
-    <Button {...props} children={props.children.split("").reverse()} />
-  );
   return (
     <div>
       <Wrapper>
@@ -64,6 +82,18 @@ export default function StyledComponents() {
         <Button as={ReversedButton}>
           Custom Button with Normal Button styles
         </Button>
+      </>
+      <>
+        <React.Fragment>
+          <Thing>Hello world!</Thing>
+          <Thing>How ya doing?</Thing>
+          <Thing className="something">The sun is shining...</Thing>
+          <div>Pretty nice day today.</div>
+          <Thing>Don't you think?</Thing>
+          <div className="something-else">
+            <Thing>Splendid.</Thing>
+          </div>
+        </React.Fragment>
       </>
     </div>
   );
